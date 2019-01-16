1d508ed1810202b4f54e880f4eb53ae9f9c3a69c
pengjian.wang@xstudy.com.cn <408648033@qq.com>
2019-01-16 11:11:44 +0800

    demo

---
+++ apps/web/controllers/DemoController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Bpai\Web\Controllers;
+class DemoController extends ControllerBase {
+
+	public function initialize() {
+	}
+	
+	public function indexAction() {
+
+//        $Images = new Imagick()
+        $path = __DIR__.'/../../../public/uploads/20181217/20181217182207821.jpg';
+        $image = new \Phalcon\Image\Adapter\Imagick($path);
+        echo '<pre>';
+
+        var_dump($image);
+        var_dump($image->getImage());
+        var_dump($image->getRealpath());
+        var_dump($image->getWidth());
+        var_dump($image->getHeight());
+
+//        var_dump($image->crop(300,300,100,50));
+//        var_dump($image->save(__DIR__.'/../../../public/uploads/20181217/1.jpg'));
+
+        exit;
+
+
+    }
+
+}
